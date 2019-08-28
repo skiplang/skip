acd5eda9cca78857f6b26621ee92e7020efe91a6
Lucas Hosseini <hoss@fb.com>
2019-08-28 05:46:27 +0200

    Remove js backend.

--- CMake/SkipOptions.cmake
+++ CMake/SkipOptions.cmake
@@ -7,7 +7,6 @@ option(USE_JEMALLOC "Use jemalloc" ON)
 # Quiet make output
 set_property(GLOBAL PROPERTY RULE_MESSAGES OFF)
 
-option(INCLUDE_JS_SELF "Include JS self hosting tests" OFF)
 option(INCLUDE_FAILING "Include failing tests" OFF)
 
 option(EXTERN_LKG "Location of an external LKG. If set should be the path to another skip repo's build tree to use as the lkg." OFF)
