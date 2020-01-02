56ed5420f192f9b72ce10ae13000bfc4ba0479a0
Julien Verlaguet <julien.verlaguet@gmail.com>
2020-01-02 14:50:37 +0000

    replace F14 with the standard library implementations

--- src/runtime/native/include/skip/map.h
+++ src/runtime/native/include/skip/map.h
@@ -7,7 +7,6 @@
 
 #pragma once
 
-#include <folly/container/F14Map.h>
 #include <functional>
 
 namespace skip {
@@ -15,10 +14,10 @@ namespace skip {
 // use this when you have non-movable values, need reference or iterator
 // stability on rehash, or want pay-as-you-go allocation for large values.
 template <class T, class U, class V = std::hash<T>, class W = std::equal_to<T>>
-using node_map = folly::F14NodeMap<T, U, V, W>;
+using node_map = std::unordered_map<T, U, V, W>;
 
 // Use this when you don't require ref/iter stability, and want to let
 // folly choose between F14ValueMap or F14VectorMap
 template <class T, class U, class V = std::hash<T>, class W = std::equal_to<T>>
-using fast_map = folly::F14FastMap<T, U, V, W>;
+using fast_map = std::unordered_map<T, U, V, W>;
 } // namespace skip
